f17fd819180334a9ee5cda83d5b5e2b6c92238d6
Ryan Murphy <github@accounts.rdmurphy.org>
2014-08-06 14:00:27 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// # Globbing
+// For performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// Use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+  grunt.registerTask('default', []);
+};
